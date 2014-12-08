bf0ff66bc769e79c1a95c354c360a7540a17fee7
rodolfoaar <rodolfo.raar@gmail.com>
2014-12-08 23:20:04 +0000

    Revert "Revert "Detail Order page updated with Quantidade recepcionada spinner""
    This reverts commit e892ee8b63ffe9d4fbdeea7746e5b86a08a39685.

---
+++ SINF_App/Models/User.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SINF_App.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+
+        /// <summary>
+        /// Checks if user with given password exists in the database
+        /// </summary>
+        /// <param name="_username">User name</param>
+        /// <param name="_password">User password</param>
+        /// <returns>True if user exist and password is correct</returns>
+        public bool IsValid(string _username, string _password)
+        {
+            /*
+            using (var cn = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename" +
+              @"='C:\Tutorials\1 - Creating a custom user login form\Creating " +
+              @"a custom user login form\App_Data\Database1.mdf';Integrated Security=True"))
+            {
+                string _sql = @"SELECT [Username] FROM [dbo].[System_Users] " +
+                       @"WHERE [Username] = @u AND [Password] = @p";
+                var cmd = new SqlCommand(_sql, cn);
+                cmd.Parameters
+                    .Add(new SqlParameter("@u", SqlDbType.NVarChar))
+                    .Value = _username;
+                cmd.Parameters
+                    .Add(new SqlParameter("@p", SqlDbType.NVarChar))
+                    .Value = Helpers.SHA1.Encode(_password);
+                cn.Open();
+                var reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    reader.Dispose();
+                    cmd.Dispose();
+                    return true;
+                }
+                else
+                {
+                    reader.Dispose();
+                    cmd.Dispose();
+                    return false;
+                }
+            }
+             */
+            return false;
+        }
+    }
+}
